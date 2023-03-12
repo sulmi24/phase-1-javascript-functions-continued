e2cc83f465c81a06844ab122a5f3913ccbefe630
sulmi24 <a.sulmi24@gmail.com>
2023-03-12 13:38:34 +0200

    Work done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+  return `This Saturday, I want to ${target}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+
+function wrapAdjective(txt = "*") {
+  return function (emphatic = "special") {
+    return `You are ${txt}${emphatic}${txt}!`;
+  };
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
